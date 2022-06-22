c3de5e01ff694df9ee87d02864aeb8d59a3018b8
Donald Onyino <58218526+porkytheblack@users.noreply.github.com>
2022-06-22 14:56:12 +0300

    changes

--- backend/db.ts
+++
@@ -1,4 +0,0 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const client = new MongoClient("mongodb+srv://don:gOJPGMmzISZFBo1Y@cluster0.kpjnu.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
-module.exports = client
